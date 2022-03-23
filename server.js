a118e56d0a0216589db99f2380092cbbe714d296
isanjit3 <isanjit3@gmail.com>
2022-03-23 12:14:20 -0400

    form submission fixed

--- server.js
+++ server.js
@@ -5,10 +5,15 @@ const PORT = 3000 || process.env.PORT;
 const mongoose = require("mongoose");
 const MONGO_URI = "mongodb://localhost:27017/hackathon_template";
 const User = require('./models/user');
+const bodyParser = require('body-parser');
 
 // setting view engine to ejs
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
+
+// body-parser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // index page
 app.get("/", async (req, res) => {
@@ -20,6 +25,23 @@ app.get("/", async (req, res) => {
 app.get("/data-display", async (req, res) => {
   const users = await User.find({})
   res.render("data_display", {users})
+});
+
+app.post("/addUser", async (req, res) => {
+  const user = new User(req.body);
+
+  const createdUser = await user.save();
+
+  res.status(200).json(createdUser);
+});
+
+app.post("/updateUser", async (req, res) => {
+
+  const user = await user.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+  }).exec()
+
+  res.status(200).json(user);
 });
 
 // Connecting to MongoDB
