43be9814c4ebe8539cdb8508e6d2ac61978e3534
isanjit3 <isanjit3@gmail.com>
2022-03-22 20:06:10 -0400

    added mongoDB

---
+++ models/user.js
@@ -0,0 +1,23 @@
+// User model
+const mongoose = require('mongoose')
+
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      required: true,
+    },
+    firstname: {
+        type: String,
+        required: true,
+    },
+    lastname: {
+        type: String,
+        required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("User", userSchema);
